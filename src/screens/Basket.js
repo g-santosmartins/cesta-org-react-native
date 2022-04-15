b62e77fc269ffccb9305f4ff7e2cd5b8a56a9305
g-santosmartins <g.santosmartins@outlook.com>
2022-04-15 20:49:52 -0300

    fix: Basket page more details were added

--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Text, Image, StyleSheet, Dimensions } from 'react-native'
+import { Text, Image, StyleSheet, Dimensions, View } from 'react-native'
 import topViewImageBackground from '../../assets/topo.png'
-import tomatoImage from '../../assets/frutas/Tomate.png'
+// import tomatoImage from '../../assets/frutas/Tomate.png'
+import farmlogo from '../../assets/logo.png'
 
 // getting the true width depending the divice you're running
 const width = Dimensions.get('screen').width
@@ -18,10 +19,25 @@ export function Basket() {
       >
         Detalhe da cesta
       </Text>
+
+      <View style={styles.basket}>
+        <Text style={styles.name}>Cesta de Verduras</Text>
+
+        <View style={styles.farm}>
+          <Image source={farmlogo} style={styles.farmImage}/>
+          <Text style={styles.farmName}>Jenny Jack Farm</Text>
+        </View>
+        <Text style={styles.description}>
+          Uma cesta com produtos selecionados
+          cuidadosamente da fazenda direto para
+          sua cozinha.
+        </Text>
+
+        <Text style={styles.price}>R$ 40</Text>
+      </View>
     </>
   )
 }
-
 
 // styling a object
 const styles = StyleSheet.create({
@@ -40,6 +56,49 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     padding: 16
+  },
+
+  basket: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+
+  name: {
+    fontSize: 26,
+    // line height
+    lineHeight: 42,
+    color: "#464646",
+    fontWeight: "bold"
+  },
+
+  farm: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+
+  farmName: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+
+  farmImage: {
+    width: 32,
+    height: 32,
+  },
+
+  description: {
+    color: "#a3a3a3",
+    fontSize: 16,
+    lineHeight: 16,
+  },
+
+  price: {
+    color: "#2A9F85",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+    marginTop: 8
   }
 })
 
