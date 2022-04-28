b0bfbce06e9355cb2ac5d7c3f77078de3f87fea5
g-santosmartins <g.santosmartins@outlook.com>
2022-04-28 07:14:28 -0300

    feat: Adding AppLoading component from expo at App.js

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ import {
   Montserrat_400Regular,
    Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
+// expo app loading import
+import AppLoading from 'expo-app-loading';
+
 // internal imports
 import  Basket from './src/screens/Basket';
 
@@ -21,7 +24,10 @@ export default function App() {
 
   // making sure to return a view if google fonts is not ready yet
   if(!loadedFont) {
-    return <View></View>
+    // return view logic
+    // return <View></View>
+    // Use this to feedback
+    return <AppLoading/>
   }
 
   return (
