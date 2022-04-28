52dff205096bb82f84fc9e18d0c252fa52cd6c44
g-santosmartins <g.santosmartins@outlook.com>
2022-04-28 07:21:21 -0300

    fix: Adding some info to the basket at mock data file

--- src/mocks/basket.js
+++ src/mocks/basket.js
@@ -1,5 +1,9 @@
 import logoFarm from '../../assets/logo.png'
-
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png'
 
 const basket = {
   top: {
@@ -11,6 +15,33 @@ const basket = {
     logoFarm: logoFarm,
     description: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.",
     price: "R$ 40,00"
+  },
+  itens: {
+    title: "Itens da Cesta",
+    lista: [
+      {
+        name: "Tomate",
+        image: tomate
+      },
+
+      {
+        name: "Brócolis",
+        image: brocolis
+      },
+
+      {
+        name: "Batata",
+        image: batata
+      },
+      {
+        name: "Pepino",
+        image: pepino,
+      },
+      {
+        name: "Abóbora",
+        image: abobora,
+      }
+    ]
   }
 }
 
