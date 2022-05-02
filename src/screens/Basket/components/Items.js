09494d19dd5f21ca4ac38ebe3a29a9047356d49c
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-01 22:35:23 -0300

    fix: List of itens has been added

--- src/screens/Basket/components/Items.js
+++ src/screens/Basket/components/Items.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { View, Image, StyleSheet} from 'react-native'
+import { borderBottomColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
 import CustomText from '../../../components/CustomText'
 
 export default function Items({ title, list}) {
   return <>
     <CustomText style={styles.title}>{title}</CustomText>
-    {list.map(({ name, image }) => {
+    {list.map(({ name, image, index }) => {
       return <>
-          <View key={name}>
-            <CustomText >{name}</CustomText>
-            <Image source={image} />
+          <View key={index} style={styles.item}>
+            {/* first item and then text */}
+            <Image source={image} style={styles.image} />
+            <CustomText  style={styles.name}>{name}</CustomText>
           </View>
       </>
     })}
@@ -21,5 +23,34 @@ const styles = StyleSheet.create( {
     color: '#464646',
     fontWeight: 'bold',
     marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
+    paddingHorizontal: 11
+
+  },
+
+  item: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ECECEC",
+    paddingVertical: 16,
+    alignItems: "center",
+    paddingHorizontal: 11
+  },
+
+  image: {
+    width: 46,
+    height: 46,
+  },
+
+
+  name: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 11,
+    color: "#464646",
+
   }
+
 })
